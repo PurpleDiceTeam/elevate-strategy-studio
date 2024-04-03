00efb8bc27d26f2423048884e1157927f4b1acaf
liendev <liensilviosanchez@gmail.com>
2024-04-03 20:32:03 -0300

    fixes in router for wordpress

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
@@ -7,10 +7,16 @@ const routes = [
         name: "home",
         component: HomeView,
     },
+    // Add the catch-all route
+    {
+        path: '/:catchAll(.*)', // Matches any path
+        name: 'catchAll',
+        component: HomeView, // Or specify another component if desired
+    }
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(), // Use hash mode instead of history mode
     base: '/gonzawordpress/', // Set the base property to the subdirectory where WordPress is installed
     routes,
 });
